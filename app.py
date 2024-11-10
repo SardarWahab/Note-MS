14c0bfde0576b1439ec522720bedaa87b732fbb2
SardarWahab <wahabsardar130@gmail.com>
2024-11-10 21:45:20 +0500

    "Updated database URI and added API endpoint for creating notes"

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 app =  Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Notes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To disable tracking modifications
 
 db = SQLAlchemy(app)
@@ -16,11 +16,26 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True, nullable=False)
     content = db.Column(db.String(120), unique=True, nullable=False)
-    
+
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({"Message":"hello world"})
+    return jsonify({"Message":"Server is running"})
 
+@app.route('/api/note', methods=['POST'])
+def create_note():
+    data = request.get_json()
+    title = data.get('title')
+    content = data.get('content')
+    print(title,content)
+    if not title:
+        return jsonify({"error": "Title is required"})
+    if not content:
+        return jsonify({"error": "Content is required"})
+    
+    new_note = Note(title=title,content=content)
+    db.session.add(new_note)
+    db.session.commit()
+    return jsonify({"Message":"Note created successfully"})
 
 
 
